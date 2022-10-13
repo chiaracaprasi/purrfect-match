51c4e502d21a58a656dd6ced32782def47b17c20
Karoline-S <karoline.amy@gmail.com>
2022-10-13 11:33:00 +1100

    build - add all_cats endpoint

---
+++ app/views/all_cats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""api end point for route /all_cats"""
+from app.views import app_views
+from flask import abort, jsonify, request
+from models.cat_details import CatDetails
+from models.cat_personalities import CatPersonalities
+
+
+@app_views.route('/all_cats', strict_slashes=False)
+def all_cats():
+    """
+    Retrieve and return all cats from the database
+    """
+
+    all_personalities = CatPersonalities.all()
+    if not all_personalities:
+        return {}
+
+    matches_dict = {}
+    for cat_personality in all_personalities:
+        id_key = str(cat_personality['details_id'])
+        matches_dict[id_key] = []
+        cat_details = CatPersonalities.get(cat_personality['details_id'])
+        if cat_details:
+            matches_dict[id_key] += [cat_details.to_dict(), cat_personality.to_dict()]
+    return jsonify(matches_dict)
