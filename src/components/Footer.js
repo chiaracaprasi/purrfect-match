d3d203051315a549062c369f85f399e1075c1c44
Chiara Caprasi <caprasichiara@gmail.com>
2022-10-14 14:42:36 +1100

    feature: change foot bg colour

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
     <>
       <div className="fixed-bottom border-0">
         <Card>
-          <Card.Body className="pb-3 bg-footer">
-            Made with ❤️
+          <Card.Body className="pb-1 bg-primary">
+            Made with ❤️ 
           </Card.Body>
         </Card>
       </div>
