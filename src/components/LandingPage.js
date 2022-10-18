36b17b5261ae8b4729f3bc0c7b7d7755e59a18a2
Chiara Caprasi <caprasichiara@gmail.com>
2022-10-18 20:13:52 +1100

    build: add styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,8 +4,13 @@ const LandingPage = () => {
   return (
     <div>
       <header className="App-header">
-        <p>Connecting humans to their purrfect owners</p>
-        <Button href="/match" variant="secondary" size="lg">
+        <p className="fs-2 text">Connecting humans to their purrfect owners</p>
+        <Button
+          href="/match"
+          variant="secondary"
+          size="lg"
+          className="fs-4 text"
+        >
           Find Your Purrfect Match
         </Button>
       </header>
