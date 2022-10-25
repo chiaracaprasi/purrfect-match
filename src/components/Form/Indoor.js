7dff3bbd1135ee64713e43cfaddfb5f939c2ba18
Chiara Caprasi <caprasichiara@gmail.com>
2022-10-25 18:16:16 +1100

    build: add animation on Home form

--- src/components/Form/Indoor.js
+++ src/components/Form/Indoor.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
 function Indoor({ formData, setFormData }) {
+  const [pulseRing, setPulseRing] = useState(0);
+  const [pulseRing2, setPulseRing2] = useState(0);
+
   return (
     <Card>
       <Card.Header>Which best describes your home and preference?</Card.Header>
@@ -23,6 +26,9 @@ function Indoor({ formData, setFormData }) {
                   src={require("../icons/indoor.png")}
                   alt="indoor"
                   className="icon"
+                  onClick={() => setPulseRing(1)}
+                  onAnimationEnd={() => setPulseRing(0)}
+                  pulseRing={pulseRing}
                 />
                 <p>I would prefer an indoor cat, or I live in a busy area</p>
               </label>
@@ -43,6 +49,9 @@ function Indoor({ formData, setFormData }) {
                   src={require("../icons/outdoor.png")}
                   alt="outdoor"
                   className="icon"
+                  onClick={() => setPulseRing2(1)}
+                  onAnimationEnd={() => setPulseRing2(0)}
+                  pulseRing={pulseRing2}
                 />
                 <p>
                   I can manage a cat that enjoys some time outdoors, and I live
